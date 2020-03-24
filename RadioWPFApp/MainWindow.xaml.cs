d089594950ded81b11eb98ad456a240b17387acd
Dushan P <dpuvaneswaran@spartaglobal.com>
2020-03-24 20:56:53 +0000

    Completed version of RADIO WPF

---
+++ RadioWPFApp/MainWindow.xaml.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace RadioWPFApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+       
+        Radio r;
+        public MainWindow()
+        {
+            InitializeComponent();
+            r = new Radio();
+            r.Read();
+
+
+        }
+
+        private void Off_Click(object sender, RoutedEventArgs e)
+        {
+            r.TurnOff();
+            OnOffTxt.Text = "Radio is off";
+        }
+
+        private void On_Click(object sender, RoutedEventArgs e)
+        {
+            r.Read();
+            r.TurnOn();
+            OnOffTxt.Text = $"{r.Play()}";
+            VolumeTxt.Text = $"{r.Volume}";
+        }
+
+        private void Channel1_Click(object sender, RoutedEventArgs e)
+        {
+            r.Channel = 1;
+            OnOffTxt.Text = $"{r.Play()}";
+
+        }
+
+        private void Channel2_Click(object sender, RoutedEventArgs e)
+        {
+            r.Channel = 2;
+            OnOffTxt.Text = $"{r.Play()}";
+
+        }
+
+        private void Channel3_Click(object sender, RoutedEventArgs e)
+        {
+            r.Channel = 3;
+            OnOffTxt.Text = $"{r.Play()}";
+
+        }
+
+        private void Channel4_Click(object sender, RoutedEventArgs e)
+        {
+            r.Channel = 4;
+            OnOffTxt.Text = $"{r.Play()}";
+
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            r.Write();
+
+        }
+
+
+        private void VolUp_Click(object sender, RoutedEventArgs e)
+        {
+            r.Volume++;
+            VolumeTxt.Text = $"Volume: {r.Volume++}";
+        }
+
+        private void VolDown_Click(object sender, RoutedEventArgs e)
+        {
+            r.Volume--;
+            VolumeTxt.Text = $"Volume: {r.Volume--}";
+        }
+    }
+}
---
+++ RadioWPFApp/Radio.cs
@@ -0,0 +1,140 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.IO;
+
+namespace RadioWPFApp
+{
+    public class Radio
+    {
+
+        // Priviate initialisation
+
+        private int _channel = 1;
+        private bool _on = false;
+        private int _volume;
+        string path = @"C:\Users\TECH-W154birm\Downloads\RadioWPF\RadioWPF\data.json";
+
+      
+        public int Channel
+        { /// get set property
+            get { return _channel; }
+            set
+            {
+                if (value >= 1 && value <= 4 && _on == true)
+                {
+                    _channel = value;
+                }
+
+            }
+
+        }
+
+
+        // Read channel method
+
+        public int ChannelRead
+        {
+            get { return _channel; }
+            set
+            {
+                if (value >= 1 && value <= 4)
+                {
+                    _channel = value;
+                }
+            }
+
+        }
+
+        
+        // VOLUME method implemented 
+        public int Volume
+        {
+            get { return _volume; }
+
+            set
+            {
+                if (value >= 0 && value <= 100)
+                {
+                    _volume = value;
+                }
+            }
+
+        }
+
+        // READ Volume method
+        public int VolumeRead
+        {
+            get { return _volume; }
+            set
+            {
+                if (value >= 0 && value <= 100)
+                {
+                    _volume = value;
+                }
+            }
+        }
+
+       
+
+        // PLAY method used if ON displays "Playing Channel ..." or else OFF
+        public string Play()
+        {
+
+
+            if (_on == true)
+            {
+                return $"Playing channel {_channel}";
+            }
+            else
+            {
+                return "radio is off";
+            }
+
+        }
+
+
+        // TURN OFF METHOD 
+
+        public void TurnOff()
+        {
+            _on = false;
+
+        }
+
+
+        // TURN ON METHOD
+        public void TurnOn()
+        {
+            _on = true;
+        }
+
+
+        public void Write()
+        {
+
+            _channel = ChannelRead;
+            _volume = VolumeRead;
+            string output = JsonConvert.SerializeObject(this);
+            File.WriteAllText(path, output);
+        }
+
+
+
+        public void Read()
+        {
+
+            string jsonfile = File.ReadAllText(path);
+            Radio deSerialised = JsonConvert.DeserializeObject<Radio>(jsonfile);
+            ChannelRead = deSerialised.ChannelRead;
+            VolumeRead = deSerialised.VolumeRead;
+            
+        }
+
+
+
+
+    }
+
+    // implement a class Radio that corresponds to the Class diagram 
+    //   and specification in the INSTRUCTIONS document in this solution   
+}
